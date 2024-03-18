780d9eb277b3706879dcda54850881adc14c37a5
haolun <hl377724@dal.ca>
2024-03-18 00:32:12 -0300

    start

---
+++ character.h
@@ -0,0 +1,25 @@
+//
+// Created by shaon on 3/18/2024.
+//
+
+#ifndef MAJIANG_CHARACTER_H
+#define MAJIANG_CHARACTER_H
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+class userCharacter{
+public:
+
+private :
+    // the name of the user
+    string name;
+    // the health point of the user
+    int healthPoint;
+    // the attack point of the user
+    int attackPoint;
+    // the defense point of the user
+    int defensePoint;
+};
+
+#endif //MAJIANG_CHARACTER_H
---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by shaon on 3/18/2024.
+//
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(){
+
+}
